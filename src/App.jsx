c3c3ad13632ee6d7162e8353a29ebdaa6d912509
ArifMiah07 <arifmiah.me322@gmail.com>
2024-03-16 20:01:19 +0600

    add tailwind css and daisyUI, creaeing component folder and fetching data, and add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react';
+import Navbar from './components/Header/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
+
+  useEffect( ()=>{
+    fetch('./recipe.json')
+    .then(res => res.json())
+    .then(data => setItems(data));
+  }, [])
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar></Navbar>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+      <h1>React Items: {items.length}</h1>
+      {
+        items.map((item, idx)=> <img key={idx} src={item.recipe_image
+        }></img>)
+      }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
